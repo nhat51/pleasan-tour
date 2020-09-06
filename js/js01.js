a11c81715b9f4e53d416867a51c90a44cfb8497f
Morphine <64981166+hellvn@users.noreply.github.com>
2020-09-06 22:11:30 +0700

    update

--- js/js01.js
+++ js/js01.js
@@ -1,3 +1,9 @@
-function booksuccess() {
-    alert("Payment successfully");
-}
+//search script
+$(document).ready(function(){
+    $("#topsearch").on("enterKey", function() {
+        var value = $(this).val().toLowerCase();
+        $("#container").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
